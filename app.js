c8e5ccec89bf9979e7e0bee76220f4420fa4e167
Andrew Garcia <runandrew@gmail.com>
2017-02-24 12:57:45 -0500

    add main application file for server

---
+++ app.js
@@ -0,0 +1,33 @@
+// Main application file
+
+// Required libraries
+const express = require('express');
+const volleyball = require('volleyball');
+const bodyParser = require('body-parser');
+
+// App creation
+const app = express();
+const PORT = 8585;
+
+// Middleware
+
+// Logging
+app.use(volleyball);
+// Parsing
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.send('Hello');
+})
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`We're online on port ${PORT}`);
+});
+
+// Error logging middleware
+app.use((err, req, res, next) => {
+    console.error(err);
+    res.status(err.status || 500).send(err.message || 'Internal server error');
+});
